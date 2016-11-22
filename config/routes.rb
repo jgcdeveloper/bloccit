11210f3ffab8c398681f642875614f18382ac29a
Jason Clegg <jgcdeveloper@gmail.com>
2016-11-22 22:03:28 +0000

    Add Routes for Post
    Generate controller for post. Refactor routes.rb. Add resource views
    for Post. Add specs for post views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  #Get Requests to create routes for our view
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+  #Get Requests to create routes for our view
+
+  get 'about' => 'welcome#about'
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'

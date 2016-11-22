bf1716040867017db4ffd59ad82e492b25475d41
Jason Clegg <jgcdeveloper@gmail.com>
2016-11-22 23:08:33 +0000

    Add Spam Filter
    Add a SPAM filter into the posts controller. This is for purpose of
    the assignment, which requires us to tag every 5th post as SPAM.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
+
+  def spamFilter(posts,filtered)
+    posts.each_with_index do |mypost, index|
+      if (index > 1 && index % filtered == 0)
+        mypost.title = 'SPAM Post'
+      end
+    end
+  end
+
   def index
     @posts = Post.all
+    @filterPosts = 5
+
+    spamFilter(@posts, @filterPosts)
+
   end
 
   def show

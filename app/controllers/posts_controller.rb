27b61b7930417c964fed24f826738786ae839d09
Jason Clegg <jgcdeveloper@gmail.com>
2016-11-25 01:16:46 +0000

    Add Edit Functionality
    Added support and tests for edit functionality in the Bloccit program.
    This will allow a post to be edited.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,11 +27,27 @@ class PostsController < ApplicationController
 
       flash.now[:alert] = "There was an error saving post. Please try again!"
       render :new
-
     end
+
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was Updated"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error updating post. Please try again!"
+      render :edit
+    end
+
   end
 
 end

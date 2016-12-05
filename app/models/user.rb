5847cd08e03e0a0084ef0740a6e33867fa23424f
Jason Clegg <jgcdeveloper@gmail.com>
2016-12-05 20:58:46 +0000

    Add User Capitalization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 
-  before_save { self.email = email.downcase if email.present? }
+  before_save {
+    self.email = email.downcase if email.present?
+    self.name = name.split(' ').each{|name| name.capitalize!}.join(' ') if name.present?
+  }
+
 
   validates :name,
     length: { minimum: 1, maximum: 100 },
@@ -11,8 +15,14 @@ class User < ActiveRecord::Base
     uniqueness: { case_sensitive: false },
     length: { minimum: 3, maximum: 254 }
 
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :password,
+    presence: true,
+    length: { minimum: 6 },
+    if: "password_digest.nil?"
+
+  validates :password,
+    length: { minimum: 6 },
+    allow_blank: true
 
   has_secure_password
 

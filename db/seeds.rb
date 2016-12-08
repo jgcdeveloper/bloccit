a2430bdd61e0618536647c019098fdee3c777db1
Jason Clegg <jgcdeveloper@gmail.com>
2016-12-08 19:47:37 +0000

    Modify seeds.rb Into Method To Allow for Customized Numbers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,39 @@
 require 'random_data'
 
+def make_seeds(seed_topics,seed_posts,seed_comments)
 # Create topics
-15.times do
-  Topic.create!(
-    name: RandomData.random_sentence,
-    description: RandomData.random_paragraph
-  )
+  seed_topics.times do
+    Topic.create!(
+      name: RandomData.random_sentence,
+      description: RandomData.random_paragraph
+      )
+    end
+
+    topics = Topic.all
+
+    # Create Posts
+  seed_posts.times do
+    Post.create!(
+      topic: topics.sample,
+      title: RandomData.random_sentence,
+      body: RandomData.random_paragraph
+      )
+    end
+
+    posts = Post.all
+
+    # Create Comments
+    seed_comments.times do
+      Comment.create!(
+        post: posts.sample,
+        body: RandomData.random_paragraph
+        )
+    end
+
+    puts "Seed Finished"
+    puts "#{Topic.count} posts created"
+    puts "#{Post.count} posts created"
+    puts "#{Comment.count} comments created"
 end
 
-topics = Topic.all
-
-# Create Posts
-50.times do
-  Post.create!(
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-  )
-end
-
-posts = Post.all
-
-# Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
-
-puts "Seed Finished"
-puts "#{Topic.count} posts created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+make_seeds(3,9,27)

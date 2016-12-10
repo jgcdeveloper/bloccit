2d686546e5fe8982f6da3a2bd3c5a018489182d9
Jason Clegg <jgcdeveloper@gmail.com>
2016-12-10 02:18:21 +0100

    Added Rank Caculation and Sort Based On It
    Create a ranking for a post based on votes and length of time since the post
    was made. Post rating will deteriorate with time.

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   def up_votes
     votes.where(value: 1).count
@@ -21,6 +21,12 @@ class Post < ActiveRecord::Base
 
   def points
     votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
   end
 
 end

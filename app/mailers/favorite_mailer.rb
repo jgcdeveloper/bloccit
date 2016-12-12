479b665c458c1aac59aa6e9af22bf5e038a466a0
Jason Clegg <jgcdeveloper@gmail.com>
2016-12-13 00:47:41 +0100

    Complete Mail Configuration and Views
    Completed configuration so that mails will send after a new comment
    has been received on a post.

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,18 @@
+class FavoriteMailer < ApplicationMailer
+
+  default  from: "jgcdeveloper@gmail.com"
+
+  def new_comment(user, post, comment)
+
+     headers["Message-ID"] = "<comments/#{comment.id}@Bloccit>"
+     headers["In-Reply-To"] = "<post/#{post.id}@Bloccit>"
+     headers["References"] = "<post/#{post.id}@Bloccit>"
+
+     @user = user
+     @post = post
+     @comment = comment
+
+     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+end
